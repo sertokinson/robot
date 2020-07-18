8574a2bc9488934f4ae83ca68097da9b709317a0
sertokinson <sertokinson@gmail.com>
2020-07-18 20:32:32 +0300

    0.22-alpha

--- robot-impl/src/main/java/ru/sertok/robot/impl/AppControllerImpl.java
+++ robot-impl/src/main/java/ru/sertok/robot/impl/AppControllerImpl.java
@@ -24,7 +24,7 @@ public class AppControllerImpl implements AppController {
     @Override
     public AppResponse version() {
         return ResponseBuilder.success(AppResponse.builder()
-                .result("0.22")
+                .result("0.22-alpha")
                 .build());
     }
 }
